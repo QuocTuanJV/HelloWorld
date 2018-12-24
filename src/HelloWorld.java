ec42c430cbb451ea8ea510c3c95d7cc42f47f323
TuanLQ <tuanlq_tt@runsystem.net>
2018-12-24 09:08:30 +0700

    commit company

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		System.out.println("Hello World 123");
 		System.out.println("Hello World 123456");
 		System.out.println("Hello World 123456789");
+		System.out.println("Hello World 123456789-123");
 	}
 
 }
