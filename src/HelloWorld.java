4effc7cab52ef54a05f312d400a2ce479b394fea
LUONG QUOC TUAN <tuanlq_tt@runsystem.net>
2019-01-04 16:03:55 +0700

    demo1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,11 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
-		System.out.println("Hello World 123");
-		System.out.println("Hello World 123456");
-		System.out.println("Hello World 123456789");
-		System.out.println("Hello World 123456789-123");
+
 		
 		System.out.println("Hello World 123456789-567");
 		System.out.println("Hello World 123456789-8910");
